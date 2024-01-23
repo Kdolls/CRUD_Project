6810063d99f79ce47e1f4bb2a7fbec50b88efac9
dolleyk <kmdolley@gmail.com>
2024-01-23 18:12:16 +0000

    datasets added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mysql.connector import Error
 import pandas as pd
 
 
-# SQL server connection function
+#   SQL server connection function
 def create_server_connection(host_name, user_name, user_password):
     connection = None
     try:
@@ -20,7 +20,7 @@ def create_server_connection(host_name, user_name, user_password):
     return connection
 
 
-# DATABASE creation function
+#   DATABASE creation function
 def create_database(connection, query):
     cursor = connection.cursor()
     try:
@@ -30,8 +30,8 @@ def create_database(connection, query):
         print(f"Error: '{err}'")
 
 
-# Query execution function
-def execute_query(connection, query):
+#   Query execution function
+def ex_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
@@ -41,97 +41,80 @@ def execute_query(connection, query):
         print(f"Error: '{err}'")
 
 
-#                       FUNCTION CALL
+#   FUNCTION CALL
 link = create_server_connection('localhost', 'root', 'password')
 # create_database(connection,'CREATE DATABASE school')
 
 
-# tables
-
-students_table = """  
+#   Tables
+#   Students table
+students_table = """
 CREATE TABLE Students (
     student_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    birth_date DATE,
+    date_of_birth DATE,
     enrollment_date DATE
 );"""
 
+#   Teachers table
 teachers_table = """
 CREATE TABLE Teachers (
     teacher_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
+    date_of_birth DATE,
     hire_date DATE
 );"""
 
+#   Courses table
 courses_table = """
 CREATE TABLE Courses (
     course_id INT PRIMARY KEY,
     course_name VARCHAR(100),
-    instructor_id INT,
-    FOREIGN KEY (instructor_id) REFERENCES Teachers(teacher_id)
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id)
 );"""
 
-
+#   Enrollments table
 enrollments_table = """
 CREATE TABLE Enrollments (
     enrollment_id INT PRIMARY KEY,
     student_id INT,
     course_id INT,
-    classroom_id INT,
     enrollment_date DATE,
     FOREIGN KEY (student_id) REFERENCES Students(student_id),
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-    FOREIGN KEY (classroom_id) REFERENCES Classrooms(classroom_id)
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
 );"""
 
-grades_table = """
-CREATE TABLE Grades (
-    grade_id INT PRIMARY KEY,
-    enrollment_id INT,
-    grade CHAR(1),
-    FOREIGN KEY (enrollment_id) REFERENCES Enrollments(enrollment_id)
-);"""
-
-sample_data_teacher = """
-    (1, 'Dr. Brown', 'Mathews', '2005-08-15'),
-    (2, 'Prof. White', 'Smith', '2010-02-20'),
-    (3, 'Dr. Johnson', 'Williams', '2018-06-10');
-"""
-
-sample_data_classrooms = """
-    (1, '101'),
-    (2, '102'),
-    (3, '201');
-"""
-sample_data_courses_values = """
-    (101, 'Mathematics', 1),
-    (102, 'English Literature', 2),
-    (103, 'Computer Science', 3);
-"""
-
-sample_data_students_values = """
+#   Sample data
+students_values = """
 INSERT INTO Students VALUES
     (1, 'John', 'Doe', '2000-01-15', '2022-09-01'),
     (2, 'Jane', 'Smith', '2001-03-22', '2022-09-01'),
-    (3, 'Mike', 'Johnson', '2000-12-05', '2022-09-01');
-"""
-sample_data_enrollments = """
-INSERT INTO Enrollments VALUES
-    (1, 1, 101, 1, '2022-09-01'),
-    (2, 1, 102, 2, '2022-09-01'),
-    (3, 2, 101, 3, '2022-09-01'),
-    (4, 2, 103, 1, '2022-09-01'),
-    (5, 3, 102, 2, '2022-09-01');
-"""
+    (3, 'Mike', 'Johnson', '2000-12-05', '2022-09-01'
+);"""
 
-sample_data_grades = """
-INSERT INTO Grades VALUES
-    (1, 1, 'A'),
-    (2, 2, 'B'),
-    (3, 3, 'B+'),
-    (4, 4, 'A-'),
-    (5, 5, 'C');
-"""
+teachers_values = """
+INSERT INTO Teachers VALUES
+    (1, 'Dr. Brown', 'Mathews', '1965-05-10', '2005-08-15'),
+    (2, 'Prof. White', 'Smith', '1978-12-20', '2010-02-20'),
+    (3, 'Dr. Johnson', 'Williams', '1985-08-31', '2018-06-10'
+);"""
+
+courses_values = """
+INSERT INTO Courses VALUES
+    (101, 'Mathematics', 1),
+    (102, 'English Literature', 2),
+    (103, 'Computer Science', 3
+);"""
+
+enrollment_values = """
+INSERT INTO Enrollments VALUES
+    (1, 1, 101, '2022-09-01'),
+    (2, 1, 102, '2022-09-01'),
+    (3, 2, 101, '2022-09-01'),
+    (4, 2, 103, '2022-09-01'),
+    (5, 3, 102, '2022-09-01'
+);"""
 
