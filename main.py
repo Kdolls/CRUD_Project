2b796e47056737f3bb453436f16720a42cb80f74
dolleyk <kmdolley@gmail.com>
2024-01-23 15:58:54 +0000

    connect to server, creat db, execute sql commands

---
+++ main.py
@@ -0,0 +1,64 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+# SQL server connection function
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+# callback function
+
+
+# DATABASE creation function
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+# Query execution function
+def execute_query(query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+# connection function call
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+
+#                       FUNCTION CALL
+connection = create_server_connection('localhost', 'root', 'password')
+create_database(connection,'CREATE DATABASE school')
+
+
